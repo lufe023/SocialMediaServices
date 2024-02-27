1ba803a0b4557c1a4bed1156989947a878ec717d
Luis Fernando <lufe023@gmail.com>
2024-02-26 21:36:50 -0400

    Ajustando la configuracion

--- src/config.js
+++ src/config.js
@@ -2,16 +2,17 @@
 require('dotenv').config()
 
 const config = {
-    port: process.env.PORT || 9000,
-    nodeEnv: process.env.NODE_ENV || 'development', //? Desarrollo, Testing, Produccion
+    port: process.env.PORT,
+    nodeEnv: process.env.NODE_ENV, //? Desarrollo, Testing, Produccion
     jwtSecret: process.env.JWT_SECRET,
+    host: process.env.HOST,
+    frontendHost: process.env.FRONTENDHOST,
     db: {
-        host: process.env.DB_HOST || 'localhost',
-        username: process.env.DB_USER || 'postgres',
-        password: process.env.DB_PASS || 'root',
-        dbName: process.env.DB_NAME
+        host: process.env.DB_HOST ,
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASS,
+        dbName: process.env.DB_NAME 
     }
-
 }
 
 module.exports = config
